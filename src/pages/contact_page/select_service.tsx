3f403c81e4c8b89527f786f436cdf315a3950d59
SenaGodsway <godswaysena1@gmail.com>
2025-06-06 04:06:57 +0000

    footer back to noramal and other ui fixes

--- src/pages/contact_page/select_service.tsx
+++ src/pages/contact_page/select_service.tsx
@@ -3,7 +3,7 @@ const SelectService = () => {
 
 
   return (
-    <div className='mx-auto w-11/12 md:w-6/12 lg:w-8/12'>
+    <div className='mx-auto w-11/12 md:w-7/12 lg:w-8/12'>
       <h1 className='mb-4 text-2xl font-bold'>What service are you interested in?</h1>
       <p className='mb-8 text-gray-600'>Select a service to get started</p>
       <div className='grid grid-cols-1 gap-6 md:grid-cols-3'>
@@ -15,10 +15,11 @@ const SelectService = () => {
             //   onClick={() => handleServiceSelect(service.id)}
               className={`flex flex-grow flex-col gap-4 rounded-lg border-2 px-4 py-8 text-left transition-all 
                 ? 'border-[#28AE9E] shadow-lg' : 'border-gray-200'} 
-                hover:border-[#28AE9E] hover:shadow-lg cursor-pointer`}
+                hover:border-black hover:shadow-lg cursor-pointer`}
             >
-              <IconComponent size={30} className='text-[#28AE9E]' />
+              <IconComponent size={30} className='' />
               <p className='font-bold'>{service.title}</p>
+              <p className='text-gray-600'>{service.description}</p>
             </div>
           );
         })}
