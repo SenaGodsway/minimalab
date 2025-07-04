08f08109e4204efa65112a86b688bb326371de95
SenaGodsway <godswaysena1@gmail.com>
2025-07-04 00:50:44 +0000

    made changes to orbit and fixed responsiveness issues

--- src/pages/landing_page/hero_section.tsx
+++ src/pages/landing_page/hero_section.tsx
@@ -3,8 +3,7 @@ import OrbitingCircle from "./orbit/orbiting_circle";
 
 const HeroSection = () => {
   return (
-    <div className="flex flex-col items-center gap-[0px] pl-0 pt-12 md:flex-row md:pl-6 md:pt-6">
-
+    <div className="flex flex-col items-center gap-[70px] pl-0 pt-12 md:flex-row md:gap-[0px] md:pl-6 md:pt-6">
       <div className="px-6 pt-16 text-black sm:w-full md:w-[45%] md:px-12">
         <h1 className="mb-12 text-[30px] font-semibold leading-10 text-black md:text-4xl">
           We Build Intelligent Digital Experiences — Engineering the Future with
@@ -19,7 +18,7 @@ const HeroSection = () => {
           <LinkButton to="/get_quote">Get a Quote</LinkButton>
         </span>
       </div>
-      <div className="mx-auto mt-4 h-[50vh] w-10/12 rounded-xl md:h-[70vh] md:w-[50%]">
+      <div className="mx-auto h-[70vh] w-[95%] overflow-hidden md:overflow-visible box-border rounded-xl md:h-[70h]  md:w-[50%]">
         {/* <Carousel /> */}
         <OrbitingCircle />
       </div>
